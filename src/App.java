b2f9c2559df778323eb5c05a060b04c85764b38b
Valeriatm06 <valeria.tocarruncho@uptc.edu.co>
2024-09-04 11:53:45 -0500

    Calculator with abstract class

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import co.edu.uptc.models.IntegerCalculator;
+import co.edu.uptc.models.Operation;
 import co.edu.uptc.test.RealCal;
 import co.edu.uptc.test.Test;
 
@@ -8,15 +10,20 @@ public class App {
      public static void main(String[] args) {
           
           List<Integer> randomNumber = generateRandomNumer(10000, 1, 10000);
+    
+          Test testObject = new Test();
 
-          Test test = new Test();
+          testObject.testArrayList(randomNumber);
 
-          test.testArrayList(randomNumber);
           RealCal<Integer> ra = new RealCal<>();
-          ra.run(test.getList(), "Array List");
 
-          test.testSimpleList(randomNumber);
-          ra.run(test.getList(), "Simple List");
+          IntegerCalculator<Integer> intCal = new IntegerCalculator<>(testObject.getList(), Operation.SUM);
+          ra.run(intCal,"ArrayLIst");
+
+          intCal.resetResult();
+    
+          testObject.testSimpleList(randomNumber);;
+          ra.run(intCal,"simpleLIst");
 
      }
 
@@ -28,24 +35,4 @@ public class App {
           }
           return list;
      }
-
-
-
-
-
-      //  SimpleList sm = new SimpleList();
-     //  long start = System.nanoTime();
-     //  for(int i = 0; i < 29000000; i++){
-     //   sm.addEnd("a");
-     //  }
-     //  long end = System.nanoTime();
-     //  System.out.println("tiempo: " + (end-start));
-     //    sm.add("hi");
-     //    sm.add("hi2");
-     //    sm.add("hi3");
-     //    sm.addToTop("hi4");
-
-     //    sm.addAfterNode("hi2", "hi5");
-
-     //    sm.showList();
 }
